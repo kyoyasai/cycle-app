aab804d41472c4896d39e4d4f9c095736a563279
kyoyasai <ktakuku14@gmail.com>
2020-12-15 22:49:42 +0900

    バリデーション設定（ユーザー管理機能）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて8文字以上で設定して下さい' }
+
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+  end
 end
