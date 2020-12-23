cf851ed5a26c07b2841eb5ad63400d91c616a536
kyoyasai <ktakuku14@gmail.com>
2020-12-23 14:32:49 +0900

    コントローラー設定（投稿編集機能）

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_article, only: [:edit, :update]
+  before_action :user_judge, only: [:edit, :update]
 
   def index
     @articles = Article.includes(:user)
@@ -19,8 +21,27 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @article.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:article).permit(:title, :prefecture_id, :distance, :content, :evalustion).merge(user_id: current_user.id)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def user_judge
+    redirect_to root_path unless current_user == @article.user
   end
 end
