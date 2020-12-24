0bd546abf83a7b803ef6ee70f3cea5f88b27d7df
kyoyasai <ktakuku14@gmail.com>
2020-12-24 14:54:45 +0900

    ルーティング設定（投稿詳細表示機能）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
-  resources :articles, only: [:new, :create, :edit, :update, :destroy]
+  resources :articles, only: [:new, :create, :edit, :update, :destroy, :show]
   resources :users, only: :show
 end
