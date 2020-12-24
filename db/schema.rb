6de7b8998abc00f1b3219a38bd61697395a8374e
kyoyasai <ktakuku14@gmail.com>
2020-12-24 15:37:36 +0900

    マイグレーション実行（commentsテーブル）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_133717) do
+ActiveRecord::Schema.define(version: 2020_12_24_062613) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_12_16_133717) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "message", null: false
+    t.bigint "user_id"
+    t.bigint "article_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -40,5 +50,6 @@ ActiveRecord::Schema.define(version: 2020_12_16_133717) do
   end
 
   add_foreign_key "articles", "users"
-
+  add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
