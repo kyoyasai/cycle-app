1a8ab4e404d469916223e4afb5649adfeeb1384e
kyoyasai <ktakuku14@gmail.com>
2020-12-24 15:58:38 +0900

    ルーティング設定（コメント機能）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "articles#index"
-  resources :articles, only: [:new, :create, :edit, :update, :destroy, :show]
+  resources :articles do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
