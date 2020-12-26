e512ab367b9a310587906aa082f0d2c2cbadb455
kyoyasai <ktakuku14@gmail.com>
2020-12-26 12:24:06 +0900

    コントローラー設定（コメント機能）

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.new(comment_params)
+    @comments = @article.comments.includes(:user)
+    if @comment.save
+      redirect_to article_path(@article)
+    else
+      render "articles/show"
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:message).merge(user_id: current_user.id, article_id: params[:article_id] )
+  end
+end
