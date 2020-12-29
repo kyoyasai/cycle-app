8143d05ab5af3146eb60628a57a66f69699e0bc8
kyoyasai <ktakuku14@gmail.com>
2020-12-29 17:29:06 +0900

    バリデーションの修正

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :article
-  validates :article_id, uniquness: { scope: :user_id }
+  validates :article_id, uniqueness: { scope: :user_id }
 end
