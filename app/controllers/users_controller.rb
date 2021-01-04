2990c9873dae557824c0a641870239419a171783
kyoyasai <ktakuku14@gmail.com>
2021-01-04 18:08:57 +0900

    コントローラー修正（投稿検索機能）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  before_action :search_product, only: :show
+
   def show
     @user = User.find(params[:id])
     @articles = @user.articles
+  end
+
+  def search
+    @user = User.find(params[:id])
+    @articles = @user.articles
+    @results = @q.result.includes(:user)
+  end
+
+  def search_product
+    @q = Article.ransack(params[:q])
   end
 end
