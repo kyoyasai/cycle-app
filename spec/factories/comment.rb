18c0a61c01628b363e0394688f56807834b382c2
kyoyasai <ktakuku14@gmail.com>
2021-01-18 22:37:26 +0900

    単体テストの日本語対応

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    message     {Faker::Lorem.sentence}
+    #message {Faker::Lorem.sentence}
     association :user
     association :article
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 describe Comment do
   before do
-    @comment = FactoryBot.build(:comment)
+    @comment = FactoryBot.build(:comment, message: Faker::Lorem.sentence)
   end
 
-  describe 'ユーザー新規登録' do
-    context '新規登録がうまくいく時' do
-      it 'message存在すれば登録できる' do
+  describe 'コメント投稿' do
+    context 'コメント投稿がうまくいく時' do
+      it 'コメント存在すれば登録できる' do
         expect(@comment).to be_valid
       end
     end
-    context '新規登録がうまくいかない時' do
-      it 'messageが空だと登録できない' do
+    context 'コメント投稿がうまくいかない時' do
+      it 'タイトルが空だと登録できない' do
         @comment.message = ""
         @comment.valid?
-        expect(@comment.errors.full_messages).to include("Messageを入力してください")
+        expect(@comment.errors.full_messages).to include("コメントを入力してください")
       end
     end
   end
