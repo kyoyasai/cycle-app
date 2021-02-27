b7f818c45508335c138a1db41b032bd63c7b966c
Takuya Karatsu <ktakuku14@gmail.com>
2021-02-27 22:02:04 +0900

    userモデルバリデーションスペルミス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,}\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて8文字以上で設定して下さい' }, on: :crteate
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて8文字以上で設定して下さい' }, on: :create
 
   with_options presence: true do
     validates :nickname
