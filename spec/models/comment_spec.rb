dc36016b618df102354262eedf2f12956124266d
Takuya Karatsu <ktakuku14@gmail.com>
2021-03-01 23:34:13 +0900

    コメントモデルのテスト編集

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,10 +12,15 @@ describe Comment do
       end
     end
     context 'コメント投稿がうまくいかない時' do
-      it 'タイトルが空だと登録できない' do
+      it 'コメントが空だと登録できない' do
         @comment.message = ""
         @comment.valid?
         expect(@comment.errors.full_messages).to include("コメントを入力してください")
+      end
+      it 'コメントが101文字以上だと登録できない' do
+        @comment.message = Faker::Lorem.characters(number: 101)
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("コメントは100文字以内で入力してください")
       end
     end
   end
